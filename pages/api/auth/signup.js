f0f4e1fdad189a366dc1bf20c7f817ce7775dd36
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-08-25 12:51:13 +0100

    signup route

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,3 +1,25 @@
-export const signupHandler = () => {
-    
-}
+import { connectDB } from "../../../lib/db";
+
+const isValidEmail = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+export const signupHandler = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const client = await connectDB();
+    const db = client.db();
+
+    if (!isValidEmail(email)) {
+      return res.status(422).json({ status: false, message: "Invalid email" });
+    }
+
+    if (!password || password.trim() === "") {
+      return res.status(422).json({ status: false, message: "Invalid input" });
+    }
+
+    const newUser = { email, password };
+    const user = db.collection("users").insertOne();
+  } catch (error) {}
+};
